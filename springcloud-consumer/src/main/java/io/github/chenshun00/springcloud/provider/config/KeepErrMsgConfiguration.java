294649922ff9af8b4d77e02d7298a09da84dc23e
chenshun00 <chenshun00@GMAIL.COM>
2022-04-29 18:57:20 +0800

    chore: 修改jdk版本，支持jdk8编译

--- springcloud-consumer/src/main/java/io/github/chenshun00/springcloud/provider/config/KeepErrMsgConfiguration.java
+++ springcloud-consumer/src/main/java/io/github/chenshun00/springcloud/provider/config/KeepErrMsgConfiguration.java
@@ -37,7 +37,7 @@ public class KeepErrMsgConfiguration {
                 String json = Util.toString(response.body().asReader(StandardCharsets.UTF_8));
                 exception = new RuntimeException(json);
                 // 将返回内容反序列化为Result，这里应根据自身项目作修改
-                Result<Void> result = JSONObject.parseObject(json, new TypeReference<>() {
+                Result<Void> result = JSONObject.parseObject(json, new TypeReference<Result<Void>>() {
                 });
                 // 业务异常抛出简单的 RuntimeException，保留原来错误信息
                 if (!result.isSuccess()) {
